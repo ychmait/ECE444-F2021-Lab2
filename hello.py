38e7125012f6d36f8fb9b9db9b70c03d6f47edc3
ychmait <90428846+ychmait@users.noreply.github.com>
2021-09-25 19:51:38 -0400

    Activity 4- step 2 complete
    Completed activity 4

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,6 +14,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email= StringField('What is your UofT email address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 
@@ -32,10 +33,15 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email address!')
         session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+        session['email'] = form.email.data
+        #return redirect(url_for('index'))
+        
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 if __name__ == '__main__':    app.run(debug=True)
